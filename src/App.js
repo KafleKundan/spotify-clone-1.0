5bb71ee82be2736aa933224170aee90bc33c47e4
kaflekundan <kundankafle1998@gmail.com>
2022-06-25 15:42:33 +0545

    added the token and the user value to DataLayer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
 import SpotifyWebApi from "spotify-web-api-js";
 import Login from "./components/Login";
 import { getTokenFromUrl } from "./spotify";
 import Player from "./components/Player";
+import { useDataLayerValue } from "./DataLayer";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
-    const [token, setToken] = useState(null);
+    // const [token, setToken] = useState(null)
+    // Rather than keeping the token here we dispatch it to the DataLayer
+
+    const [{user,token}, dispatch] = useDataLayerValue();
+
     useEffect(() => {
         const hash = getTokenFromUrl();
         // Removing the token from the URL
@@ -16,16 +21,26 @@ function App() {
         const _token = hash.access_token;
 
         if (_token) {
-            setToken(_token);
-
+            // Adding the token to the DataLayer
+            dispatch({
+                type:"SET_TOKEN",
+                token: _token,
+            })
             // Accessing the token to get Data
             spotify.setAccessToken(_token);
             // Getting user information
-            spotify.getMe().then((user) => {
-                console.log(":person: ", user);
+            spotify.getMe().then((_user) => {
+                dispatch({
+                    type: "SET_USER",
+                    // user variable of DataLayer value = username from the spotify
+                    user: _user,
+                });
             });
         }
-    }, []);
+        console.log(user)
+    },[]);
+
+
     return (
         <div className="app">
             {/* Renders the login page if token is not available */}
@@ -36,4 +51,4 @@ function App() {
 
 export default App;
 
-// 1:33:09
+// 2:13:38
--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,5 @@
 export const initialState = {
+    token: null,
     user : null,
     playlists: [],
     playing: false,
@@ -8,12 +9,17 @@ const reducer = (state, action)=>{
     console.log(action)
 
     // Action has two values 
-    // Action => type, [payload]
+    // Action => type, [payload(or value)]
     switch(action.type){
         case 'SET_USER':
             return{
                 ...state,
                 user: action.user,
+            }
+        case 'SET_TOKEN':
+            return{
+                ...state,
+                token: action.token,
             }
         default:
             return state
