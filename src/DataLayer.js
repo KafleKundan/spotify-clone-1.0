4e90beaaa408d7787d867ffdfa967b6318be35ab
kaflekundan <kundankafle1998@gmail.com>
2022-06-25 15:11:13 +0545

    context api was finally set up

--- src/DataLayer.js
+++ src/DataLayer.js
@@ -7,3 +7,5 @@ export const DataLayer = ({initialState, reducer, children})=>(
         {children}
     </DataLayerContext.Consumer.Provider>
 );
+
+export const useDataLayerValue = ()=> useContext(DataLayerContext)
